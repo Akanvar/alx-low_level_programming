07bfd4ed04cad97fa4d75edad481bf32a200d30a
Akanvar <kelvinawiapo9@gmail.com>
2023-09-13 03:01:45 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @i: first integer value
+ * @j: second integer value
+ * Return: integer value
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - Entry point to code
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = (a * b);
+			_putchar(c + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
