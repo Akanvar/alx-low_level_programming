d0adc04397e8c00a8c857846ca0557eeaa6093f9
Akanvar <kelvinawiapo9@gmail.com>
2023-09-14 23:48:22 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: character to be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 1;
+	while
+		(n > 0) {
+			for (i = n; i <= n; i++)
+				_putchar('_');
+			n--;
+		}
+	_putchar('\n');
+}
