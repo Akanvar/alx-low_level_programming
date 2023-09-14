ff0e121b8c1681c39f8c7931710d45efa808b08f
Akanvar <kelvinawiapo9@gmail.com>
2023-09-14 23:13:53 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while
+		(i < 10) {
+			for (j = 0; j < 15; j++)
+			{
+				if (j > 9)
+					_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			_putchar('\n');
+			i++;
+		}
+}
