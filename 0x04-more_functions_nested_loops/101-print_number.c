f7538c436410386cb6c7223c9000c218a67bc7a6
Akanvar <kelvinawiapo9@gmail.com>
2023-09-15 02:12:06 +0000

    101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - Entry point to print integers
+ * @n: integer to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
