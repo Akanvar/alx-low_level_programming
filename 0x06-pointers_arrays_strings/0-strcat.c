d4cf30a42a6a36e713740668b6ac62a5d4a43893
Akanvar <kelvinawiapo9@gmail.com>
2023-09-22 12:23:34 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strcat - concartenate two strings
+ * @dest: input string
+ * @src: input string
+ * Return: character
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while
+		(dest[i] != '\0') {
+			i++;
+		}
+	j = 0;
+	while
+		(src[j] != '\0') {
+			src[j] = dest[i];
+			i++;
+			j++;
+		}
+	dest[i] = '\0';
+	return (dest);
+}
