dc8c141b3aba5b18192d3eb3a8373a7a8228217b
Akanvar <kelvinawiapo9@gmail.com>
2023-09-26 01:32:25 +0000

    modified 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i, j = n;
+	unsigned int i;
 
-	for (i = 0; i < j--; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	return (dest);
 }
