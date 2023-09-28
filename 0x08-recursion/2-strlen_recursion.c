0ec6109a05b8ecfa8e169fae3e2ef696964b342f
Akanvar <kelvinawiapo9@gmail.com>
2023-09-28 02:36:32 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion -  returns the length of a string
+ * @s: string whose length is return
+ * Return: integer value of length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
+}
