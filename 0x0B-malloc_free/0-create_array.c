7b567011e83d9fc06eb45efb034d3a892c6e1eb4
Akanvar <kelvinawiapo9@gmail.com>
2023-10-03 16:24:05 +0000

    modified to include condition for if size = 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,8 +8,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+
 	unsigned int n;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
