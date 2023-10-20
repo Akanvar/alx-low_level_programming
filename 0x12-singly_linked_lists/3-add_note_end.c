84a8c6c96b3c85f50dbcec992c91f50d42d9e980
Akanvar <kelvinawiapo9@gmail.com>
2023-10-20 05:40:25 +0000

    Task 3

---
+++ 0x12-singly_linked_lists/3-add_note_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node_end - adds not at the end of list
+ * @head: double pointer to list
+ * @str: string to be duplicated
+ * Return: address of node, else NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
+
