7a1fff556049de3f3b9b3375f33e89e871a887af
Akanvar <kelvinawiapo9@gmail.com>
2023-10-24 22:44:56 +0000

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,7 +5,7 @@
  * @n: data to be inserted
  * Return: Address of new element, else NULL
  */
-list_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
