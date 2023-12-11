75cf8fff0fc6c1f6c65b21dd7e7cf6264de2cdde
Akanvar <kelvinawiapo9@gmail.com>
2023-12-11 13:14:25 +0000

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns number of elements in a doubly linked list
+ * @h: constant head pointer
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (len);
+	while (temp)
+	{
+		len += 1;
+		temp = temp->next;
+	}
+	
+	return (len);
+}
